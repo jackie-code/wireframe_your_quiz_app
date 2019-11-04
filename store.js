b14ba67584a3aac9f4ba02b88344893707825d2a
Jacqeuline Martin <paradiseprofessional88@gmail.com>
2019-11-04 16:28:50 -0500

    update the index.html and store.js

---
+++ store.js
@@ -0,0 +1,88 @@
+const STORE = {
+    questions: [//1
+      {
+        question: "Are GMO's healthy?",
+        options: [
+          "yes",
+          "no"
+        ],
+        answer: "no"
+      },
+      //2
+      {
+        question: "WDoes GMO stand for genetically modified organism?",
+        options: [
+            "yes",
+            "no"
+        ],
+        answer: "yes"
+      },
+      //3
+      {
+        question: "GMO's are currently required to be labeled in Europe. Is it currently required in the United States?",
+        options: [
+            "yes",
+            "no"
+        ],
+        answer: "no"
+      },
+      //4
+      {
+        question: "Is there a link between GMO's and infertility?	", 
+        options: [
+            "yes",
+            "no"
+        ],
+        answer: "yes"
+      },
+      //5
+      {
+        question: "Can GMO's cause digestive problems, fatigue, obesity, clouding of consciousness (brain fog). mood problems/anxiety/depression, food allergies or sensitivities, memory and concentration, Joint pain, seasonal allergies, gluten sensitivities, and insomnia? ",
+        options: [
+            "yes",
+            "no"
+        ],
+        answer: "yes"
+      },
+      //6
+      {
+        question: "Do GMO's mean something is organic?",
+        options: [
+            "yes",
+            "no"
+        ],
+        answer: "no"
+      },
+      //7
+      {
+        question: 'Are GMO health risks just a rumor',
+        options: [
+            "yes",
+            "no"
+        ],
+        answer: "no"
+      },
+      //8
+      {
+        question: "Are the most common GMO sources from soybean, maze, cotton and canola?", 
+        options: [
+            "yes",
+            "no"
+        ],
+        answer: "yes"
+      },
+      //9
+      {
+        question: "Do GMO foods have there genome modified in a way that would otherwise not happen naturally?",
+        options: [
+            "yes",
+            "no"
+        ], 
+        answer: "yes"
+      },
+      //10
+      
+    ],
+    currentQuestion: 0,
+    score: 0
+  };
